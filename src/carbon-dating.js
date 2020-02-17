4a2aed2f4f3b2f41ecf781dd8c5f51561cfa3dc9
AlexeyTeterin <teterin.alexey@gmail.com>
2020-02-17 17:38:34 +0500

    fix: remove console log in func carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,7 +6,6 @@ module.exports = function dateSample(sampleActivity) {
   if (sampleActivity.match(/[\d]+[.][\d]+[.]/)) sampleActivity =
       sampleActivity.slice(0,sampleActivity.lastIndexOf("."))+
       sampleActivity.slice(sampleActivity.lastIndexOf(".")+1,sampleActivity.length-1);
-  console.log(sampleActivity);
   if (isNaN(Number(sampleActivity))) return false;
   if (sampleActivity>=15||sampleActivity<=0) return false;
   return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD)));
