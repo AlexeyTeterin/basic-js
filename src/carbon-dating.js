14f8177c7a44141cf1cfcc56b61b4efd674f7469
AlexeyTeterin <teterin.alexey@gmail.com>
2020-02-17 15:15:15 +0500

    fix: carbon-dating - add incorrect parameters handling

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,8 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  let result = Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD)));
-  return result;
+  if (typeof sampleActivity != "string") return false;
+  if (isNaN(Number(sampleActivity))) return false;
+  if (sampleActivity>15||sampleActivity<=0) return false;
+  return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity)/(0.693/HALF_LIFE_PERIOD)));
 };
