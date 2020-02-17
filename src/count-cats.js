a59766167c54c4afc77bc84f7ba87caf6147bf10
AlexeyTeterin <teterin.alexey@gmail.com>
2020-02-17 14:01:59 +0500

    feat: solved count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if (matrix==null) return 0;
+  let sum=0;
+  for (let x of matrix) {
+    for (let y of x){
+      if (y=="^^") sum++;
+    }
+  }
+  return sum;
 };
