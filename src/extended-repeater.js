5207bbe09a5866d426da775e63dd5b57a3a6d10d
AlexeyTeterin <teterin.alexey@gmail.com>
2020-02-19 13:16:27 +0500

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,26 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let result = '';
+
+    (options.repeatTimes===undefined) ? options.repeatTimes=1 : null;
+    (options.addition===null) ? options.addition='null' : null;
+
+    for (let i = 0; i < options.repeatTimes; i++) {
+        result += str;
+
+        if (options.addition != undefined) {
+            if (options.additionRepeatTimes != undefined) {
+                for (let j = 0; j < options.additionRepeatTimes; j++) {
+                    (j == options.additionRepeatTimes - 1) ?
+                        result += options.addition :
+                        result += options.addition + options.additionSeparator;
+                }
+            } else {
+                result += options.addition;
+            }
+        }
+
+        (options.separator == undefined) ? options.separator="+" : null;
+        (i < options.repeatTimes - 1) ? result += options.separator : null;
+    }
+    return result;
 };
-  
